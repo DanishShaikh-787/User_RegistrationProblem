b5cf06c8b19c6a56cf3993630f5d345dfc0d1838
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-17 00:13:44 +0530

    Added : Check Valid Mobile Number

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,25 +1,29 @@
 #! /bin/bash -x
 
-echo " 1) Check First Name Valid Or Not : "
-echo " 2) Check Last Name Valid Or Not  : "
-echo " 3) Check Email Valid Or Not      : "
-
+echo " 1) Check First Name Valid Or Not    : "
+echo " 2) Check Last Name Valid Or Not     : "
+echo " 3) Check Email Valid Or Not         : "
+echo " 4) Check Mobile Number Valid Or Not : "
 
 read -p "Enter Choice   : " choice
 read -p "Read From User : " word
 
 case $choice in
 	1)
-#First name starts with Cap and has minimum 3 characters
+	#First name starts with Cap and has minimum 3 characters
 	pattern='^[A-Z]{1}[a-zA-Z]{2,}$'
 	;;
 	2)
-#Last name starts with Cap and has minimum 3 characters
+	#Last name starts with Cap and has minimum 3 characters
 	pattern='^[A-Z]{1}[a-zA-Z]{2,}$'
 	;;
 	3)
-#Email Valid Or NOT	
+	#Email Valid Or NOT
 	pattern='^[a-zA-Z0-9]+([.\-+]?[a-zA-Z0-9]+)*[@][a-z]{2,5}[.]?[a-z]{2,3}([.][a-z]{2})*$'
+	;;
+	4)
+	#Mobile Number Valid Or NOT
+	pattern='^(91)[ ][0-9]{10}$'
 	;;
 	*)
 	echo "Check Valid Choice"
