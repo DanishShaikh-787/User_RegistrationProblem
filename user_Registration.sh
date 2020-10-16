ab80e50381fdd2fe9d7b85220e1d350f1fe6608b
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-17 00:07:01 +0530

    Added : User Valid Email ID

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,10 +1,12 @@
 #! /bin/bash -x
 
 echo " 1) Check First Name Valid Or Not : "
-echo " 2) Check Last Name Valid Or Not : "
+echo " 2) Check Last Name Valid Or Not  : "
+echo " 3) Check Email Valid Or Not      : "
 
-read -p "Enter Choice : " choice
-read -p "User Name : " word
+
+read -p "Enter Choice   : " choice
+read -p "Read From User : " word
 
 case $choice in
 	1)
@@ -14,6 +16,10 @@ case $choice in
 	2)
 #Last name starts with Cap and has minimum 3 characters
 	pattern='^[A-Z]{1}[a-zA-Z]{2,}$'
+	;;
+	3)
+#Email Valid Or NOT	
+	pattern='^[a-zA-Z0-9]+([.\-+]?[a-zA-Z0-9]+)*[@][a-z]{2,5}[.]?[a-z]{2,3}([.][a-z]{2})*$'
 	;;
 	*)
 	echo "Check Valid Choice"
