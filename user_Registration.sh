1d1811614ea794509bbf2c3991e914bc90c29faf
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-16 23:44:08 +0530

    Added : Valid User First Name

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,4 +1,14 @@
 #! /bin/bash -x
 
-echo "Welcome To User Registration "
+#First name starts with Cap and has minimum 3 characters
 
+read -p "User Name : " word
+
+pattern1='^[A-Z]{1}[a-zA-Z]{2,}$'
+
+if [[ $word =~ $pattern1 ]]
+then
+        echo valid
+else
+        echo invalid
+fi
